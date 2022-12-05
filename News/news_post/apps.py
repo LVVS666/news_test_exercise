1b3bad06326c19c62f3c2c2e9eb77a47d33e872c
Василий Логинов <pupok777vasya@gmail.com>
2022-12-05 12:07:44 +0700

    del

--- News/news_post/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class NewsPostConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'news_post'
--- News/news_post/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.news_post,name='news_post'),
-]
